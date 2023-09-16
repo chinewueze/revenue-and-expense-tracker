849302ee62c738fbc3cb6d8dcbce880d3966b72a
chinewueze <chiderauba83@gmail.com>
2023-09-16 23:49:55 +0100

    Added service worker for PWA support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
@@ -13,7 +14,7 @@ root.render(
       </BrowserRouter>
   </React.StrictMode>
 );
-
+// serviceWorker.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
